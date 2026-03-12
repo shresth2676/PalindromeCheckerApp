59b1e915cfef7ffce367eab0700d9af90e2cb157
shresth2676 <shresth2676@gmail.com>
2026-03-12 09:52:40 +0530

    UC6

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,33 +1,34 @@
-
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+
 public class PalindromeCheckerApp {
-    public static boolean isPalindrome(String input) {
+
+    public static void main(String[] args) {
+
+        String input = "madam";
+
         Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
 
         for (char c : input.toCharArray()) {
             stack.push(c);
+            queue.add(c);
         }
 
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
-                return false;
+        boolean isPalindrome = true;
+
+        while (!stack.isEmpty() && !queue.isEmpty()) {
+            if (stack.pop() != queue.remove()) {
+                isPalindrome = false;
+                break;
             }
         }
 
-        return true;
-    }
-
-    public static void main(String[] args) {
-        String str = "madam";
-
-        if (isPalindrome(str)) {
+        if (isPalindrome) {
             System.out.println("Palindrome");
         } else {
             System.out.println("Not a Palindrome");
         }
     }
 }
-}
-
-
-
